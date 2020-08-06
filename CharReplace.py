b1c09eb7cb67ab5934105a15d78445ed07f6859e
Abhijeet Singh <abhijeetsingh.aau@gmail.com>
2020-08-06 11:14:30 +0200

    Update CharReplace.py

--- CharReplace.py
+++ CharReplace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Script:       CharReplace.py
-# date:         ons  5 aug 2020 16:00:03 CEST
+# date:         tor  6 aug 2020 11:08:36 CEST
 # modified by:  Abhjeet SIngh
 
 # imports
@@ -10,8 +10,10 @@ import sys
 
 # help menu
 if sys.argv[1] == str('-h'):
+    print("\nCharReplace: a multipurpose program to replace special characters in file/directories names or rename files/directories")
+    print("Author: Abhijeet Singh, abhijeetsingh.aau@gmail.com\n\n")
     print("\n[EXAMPLE]: python3 CharReplace.py <directory/input_dir> <Char_to_replace, 1= one, 2= many> <what_to_replace> <what_to_replace_with>")
-    print("[EXAMPLE]: python3 CharReplace.py <input_dir> 1 ' ' '_'")
+    print("\n[EXAMPLE]: python3 CharReplace.py <input_dir> 1 ' ' '_'")
     print("[EXAMPLE]: python3 CharReplace.py <input_dir> 2 '.)[(])/ ' '_'\n")
     exit()
 else:
@@ -35,22 +37,27 @@ elif char_to_replace > 1:
     replace_what_list = list(replace_what)
 
 #
-print("Replacing " + str(replace_what_list) + " in " + str(pathabs))
+
+print("." * 50)
+print("Replacing \"" + str(replace_what_list) + "\" with \"" + str(replace_with) + \
+    "\" from files/directories in path  \n\t=> " + str(pathabs))
+print("." * 50)
+
 
 #replace and rename files
-for f in os.listdir(pathabs):
+for whatword in os.listdir(pathabs):
     if char_to_replace == 1:
-        r = f.replace(replace_what_list, replace_with)
-        if (r != f):
+        withword = whatword.replace(replace_what_list, replace_with)
+        if (withword != whatword):
             try:
-                os.rename(f,r)
+                os.rename(whatword,withword)
             except:
                 pass
     elif char_to_replace > 1:
         for char in replace_what_list:
-            r = f.replace(char, replace_with)
-            if (r != f):
+            withword = whatword.replace(char, replace_with)
+            if (withword != whatword):
                 try:
-                    os.rename(f,r)
+                    os.rename(whatword,withword)
                 except:
                     pass
